877a835588892440b4b1360f703d59da6c07b60e
Sergey Semyonov <sergey.semenov@workato.com>
2023-01-11 02:11:52 +0400

    Use reviewdog/action-rubocop@master

--- rubocop/index.rb
+++ rubocop/index.rb
@@ -1,6 +1,6 @@
 
-if (2 > 1)   
-  # longc dddd ommentlongcommentlongcommentlongcommentlongcommentlongcommentlongcommentlongcommentlongcomment
+if (3 > 1)   
+  #l xx ongc dddd ommentlongcommentlongcommentlongcommentlongcommentlongcommentlongcommentlongcommentlongcomment
   p 'rubocop!!!!'
 
 end
